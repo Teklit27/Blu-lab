3eb9ec42c7d28752c8b7230dc3b6922e028855da
Teklit27 <teklitfisseha27@gmail.com>
2023-09-17 12:40:20 +0300

    the new ui section is implemented

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useTransition, animated, config } from "react-spring";
 
+import logo from '../assets/Isms.png'
+
 const descriptions = [
   {
-    leftDescription: "Avoid The Tiring Paperworks",
+    rightTitle:"Organized Data In Few Clicks", 
     rightDescription:
-      "Effortless Paperwork Handling Effortless Paperwork Handling Effortless Paperwork Handling",
-    leftColor: "bg-red-200",
+      "Our system utilizes state of the art data visualization techinques to present the information with beautifull charts and graphs with ease ",
+    rightColor: "bg-green-200",
   },
   {
-    leftDescription: "Quality Customer Service",
-    rightDescription: "Exceptional Customer Support",
-    leftColor: "bg-blue-200",
+    rightTitle:"Avoid The Tiring Paperworks",
+    rightDescription: "Our system can be accessed from your smart phone or any computer devices. Making it easy to forward any information to everyone. ",
+    rightColor: "bg-blue-200",
+  },
+  {
+    rightTitle:"Send Notification To Any Group",
+    rightDescription: "Sending notifications has never been easier. You can now send out a brodcast about new annoucments to everyone, specfic group or indiviuals with ease.",
+    rightColor: "bg-red-100",
   },
   // Add more descriptions as needed
 ];
@@ -19,37 +26,42 @@ const descriptions = [
 export default function Products() {
   const [index, setIndex] = useState(0);
 
-  // Auto-increment the index every 2 seconds to switch descriptions
+  // Auto-increment the index every 2.6 seconds to switch descriptions
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % descriptions.length);
-    }, 2000);
+    }, 2600);
 
     return () => clearInterval(interval);
   }, []);
 
   const transitions = useTransition(descriptions[index], {
     key: index,
-    from: { opacity: 0, transform: "translateX(100%)" },
-    enter: { opacity: 1, transform: "translateX(0%)" },
-    // leave: { opacity: 0, transform: "translateX(100%)" },
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    // leave: { opacity: 0 },
     config: config.default,
   });
 
   return (
-    <div className="products h-40 px-32 flex items-center justify-center">
-      {transitions(({ opacity, transform }, item) => (
+    <div className=" px-4 md:mx-48 md:px-32 md:flex  items-center justify-center">
+      {transitions(({ opacity }, item) => (
         <>
           <div
-            className="w-2/5 h-48 p-6 rounded-lg shadow-lg rounded-r-none bg-indigo-500 "
+            className={`w-full md:w-3/5 h-52 p-4 rounded-lg shadow-lg bg-indigo-400 md:rounded-r-none`}
           >
-            {descriptions[0].leftDescription}
+            <div className=" flex align-middle items-center">
+            <img src={logo} alt="Logo" className="h-14 " />
+            <h2 className="font-bold text-xl items-center text-center flex-1 text-white">Our latest product</h2>
+            </div>
+            <p className="text- "><span className="font-bold">ISMS.et</span> provides a modern way to learning. We provide a Educational managment system that is easy to use and minimizes work overheads for schools and parents alike. </p>
           </div>
           <animated.div
-            style={{ opacity, transform }}
-            className={`w-2/5 h-48 p-6  border border-gray-200 rounded-lg rounded-l-none shadow-lg ${item.leftColor}`}
+            style={{ opacity }}
+            className={`w-full mt-4 md:mt-0 md:w-3/4 h-52 p-6 text-center md:px-14 border border-gray-200 rounded-lg shadow-lg md:rounded-l-none ${item.rightColor}`}
           >
-            {item.rightDescription}
+            <h1 className="font-bold text-xl items-center text-center flex-1 text-white">{item.rightTitle}</h1>
+            <h1 className="mt-5">{item.rightDescription}</h1>
           </animated.div>
         </>
       ))}
